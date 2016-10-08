cf9066eb35babea450d02cbddd0be881cb13faa3
å <taka0206@me.com>
2016-10-08 15:21:12 +0900

    更新

--- sampleapp.js
+++ sampleapp.js
@@ -1,11 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 server.on('request', doRequest);
-server.listen(1234);
+server.listen(1337);
 
 function doRequest(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Hello');
-	res.end();
+	fs.readFile('./index.html', 'UTF-8', doRead);
+	var title = "サンプルページ";
+	var msg = "これはプログラムで用意したメッセージです.";
+
+	function doRead(err, data) {
+		var str = data.replace(/@@@title@@@/g, title).replace(/@@@message@@@/, msg);
+		res.setHeader('Content-Type','text/html');
+		res.write(str);
+		res.end();
+	}
 }
+console.log('Server running at http://127.0.0.1:1337/');
